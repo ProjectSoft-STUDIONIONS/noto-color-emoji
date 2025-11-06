89e7b9aff57b9ac9dab96cbc124181dc4f1f23f5
ProjectSoft <projectsoft2009@yandex.ru>
2025-11-06 10:48:17 +0400

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
 					ieCompat: false,
 					plugins: [],
 					modifyVars: {
-						fontpath: "../fonts"
+						fontpath: "fonts"
 					},
 				},
 				files : {
